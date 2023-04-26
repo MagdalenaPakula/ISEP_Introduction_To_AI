9616d316c3375f20d9142e30344ac62467633c6b
Magdalena Pakuła <magdalena.pakula001@gmail.com>
2023-04-26 22:33:17 +0200

    Lab 3 - finished

--- LAB3/LAB3/LAB3.py
+++ LAB3/LAB3/LAB3.py
@@ -1,9 +1,11 @@
-from pgmpy.models import BayesianModel
-
-from pgmpy.factors.discrete import TabularCPD
+from statistics import variance
+from pgmpy.models import BayesianNetwork
+from pgmpy.factors.discrete import TabularCPD 
+from pgmpy.inference import VariableElimination 
+import numpy as np 
 
 # Create the Bayesian network
-bayesNet = BayesianModel()
+bayesNet = BayesianNetwork()
 
 # Add nodes
 bayesNet.add_node("L")
@@ -19,20 +21,27 @@ bayesNet.add_edge("N", "L")
 bayesNet.add_edge("N", "I")
 bayesNet.add_edge("N", "S")
 bayesNet.add_edge("N", "R")
+    
+# Defining conditional probability distributions (CPDs)
+cpd_L = TabularCPD(variable="L", variable_card=2, values=[[0.92], [0.08]])
+cpd_I = TabularCPD(variable="I", variable_card=2, values=[[0.79], [0.21]])
+cpd_S = TabularCPD(variable="S", variable_card=2, values=[[0.88], [0.12]])
+cpd_N = TabularCPD(variable="N", variable_card=2,
+                   values=[[0.97, 0.83, 0.92, 0.78, 0.27, 0.21, 0.12, 0.08],
+                           [0.03, 0.17, 0.08, 0.22, 0.73, 0.79, 0.88, 0.92]],
+                   evidence=["L", "S", "I"], evidence_card=[2, 2, 2])
+cpd_R = TabularCPD(variable="R", variable_card=2,
+                   values=[[0.95, 0.84, 0.92, 0.62], [0.05, 0.16, 0.08, 0.38]],
+                   evidence=["N", "S"], evidence_card=[2, 2])
 
-# Define conditional probability distributions (CPDs)
-cpd_l = TabularCPD("L", 2, [[0.7, 0.3]])
-cpd_i = TabularCPD("I", 2, [[0.2, 0.9, 0.3, 0.7],
-                            [0.8, 0.1, 0.7, 0.3]],
-                   evidence=["L", "S"], evidence_card=[2, 2])
-cpd_s = TabularCPD("S", 2, [[0.95, 0.05]])
-cpd_n = TabularCPD("N", 2, [[0.9, 0.1]])
-cpd_r = TabularCPD("R", 2, [[0.99, 0.01]],
-                   evidence=["N"], evidence_card=[2])
-
-# Add CPDs to the network
-bayesNet.add_cpds(cpd_l, cpd_i, cpd_s, cpd_n, cpd_r)
+# Add CPDs to the network   
+bayesNet.add_cpds(cpd_L, cpd_I, cpd_S, cpd_N, cpd_R)
 
 # Check if the network structure and CPDs are valid
-bayesNet.check_model()
+#print(bayesNet.check_model())
 
+#Solver
+solver=VariableElimination(bayesNet)
+
+result_N = solver.query(variables="N")
+print(result_N)
